3dd2468e106e4539c0aba0706687a788c5e604e6
Zhu Yuzhou <space.fd@gmail.com>
2017-03-04 19:14:31 +1100

    Add views

---
+++ Traffic Lights/Traffic Lights/Modules/Views/LightView.swift
@@ -0,0 +1,35 @@
+//
+//  LightView.swift
+//  Traffic Lights
+//
+//  Created by Zack Zhu on 04/03/2017.
+//
+//
+
+import UIKit
+
+class LightView: UIView {
+    var light: ColorLight? {
+        didSet {
+            layer.borderColor = light?.color.cgColor
+        
+            light?.stateChanged = { [unowned self] isOn in
+                self.updateUI()
+            }
+            
+            updateUI()
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.size.width / 2
+    }
+    
+    private func updateUI() {
+        if let light = light {
+            layer.backgroundColor = light.isOn ? light.color.cgColor : nil
+        }
+    }
+    
+}
---
+++ Traffic Lights/Traffic Lights/Modules/Views/RoadView.swift
@@ -0,0 +1,9 @@
+//
+//  RoadView.swift
+//  Traffic Lights
+//
+//  Created by Zack Zhu on 04/03/2017.
+//
+//
+
+import Foundation
